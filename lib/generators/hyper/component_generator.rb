111929642227e93ccc81267251cd9216037ab079
Julien 'Lta' BALLET <contact@lta.io>
2018-01-27 12:25:54 +0100

    Replace `indet` by `indent` in template
    Signed-off-by: Julien 'Lta' BALLET <contact@lta.io>

--- lib/generators/hyper/component_generator.rb
+++ lib/generators/hyper/component_generator.rb
@@ -9,7 +9,7 @@ module Hyper
         component_array = component.split('::')
         @modules = component_array[0..-2]
         @file_name = component_array.last
-        @indet = 1
+        @indent = 1
         template 'component_template.rb',
                  File.join('app/hyperloop/components',
                            @modules.map(&:downcase).join('/'),
